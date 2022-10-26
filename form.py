4df4c7c50593628219e631c755dd2dbc59e5d758
anubhabreveals <61702730+anubhabreveals@users.noreply.github.com>
2022-10-26 23:43:13 +0530

    update

--- form.py
+++ form.py
@@ -7,13 +7,13 @@ import pandas as pd
 # Add some color to the window
 sg.theme('DarkTeal9')
 
+
 EXCEL_FILE = 'Data_Entry.xlsx'
 df = pd.read_excel(EXCEL_FILE)
 
-
-
 layout = [
     [sg.Text('Please fill out the following fields:')],
+    [sg.Text('Concept ID', size=(15,1)), sg.Combo(['Concept 1', 'Concept 2', 'Concept 3', 'Concept 4', 'Concept 5', 'Concept 6', 'Concept 7', 'Concept 8', 'Concept 9', 'Concept 10'], key='Concept ID')],
     [sg.Text('Action', size=(15,1)), sg.InputText(key='Action')],
     [sg.Text('State Change', size=(15,1)), sg.InputText(key='State Change')],
     [sg.Text('Phenomena', size=(15,1)), sg.InputText(key='Phenomena')],
@@ -31,6 +31,13 @@ def clear_input():
         window[key]('')
     return None
 
+def remove_duplicates(duplist):
+    noduplist = []
+    for element in duplist:
+        if element not in noduplist:
+            noduplist.append(element)
+
+    return noduplist
 
 while True:
     event, values = window.read()
@@ -45,10 +52,44 @@ while True:
         sg.popup('Data saved!')
         clear_input()
     if event == 'Calculate':
-        wb = load_workbook('data_Entry.xlsx')
-        ws = wb.active
-        column_action = ws['A']
-        for cell in column_action:
-            print(cell.value)
+        i=1
+        j=2
+        num_i = 'Concept '+str(i)
+        num_j = 'Concept '+str(j)
+        df_new_i = df[df['Concept ID'] == num_i]
+        df_new_j = df[df['Concept ID'] == num_j]
+
+        list_a = list(df_new_i['Action']) + list(df_new_j['Action'])
+        u_a = len(remove_duplicates(list_a))
+        n_a = len(list_a)
+
+        list_s = list(df_new_i['State Change']) + list(df_new_j['State Change'])
+        u_s = len(remove_duplicates(list_s))
+        n_s = len(list_s)
+
+        list_ph = list(df_new_i['Phenomena']) + list(df_new_j['Phenomena'])
+        u_ph = len(remove_duplicates(list_ph))
+        n_ph = len(list_ph)
+
+        list_e = list(df_new_i['Physical effect']) + list(df_new_j['Physical effect'])
+        u_e = len(remove_duplicates(list_e))
+        n_e = len(list_e)
+
+        list_r = list(df_new_i['oRgan']) + list(df_new_j['oRgan'])
+        u_r = len(remove_duplicates(list_r))
+        n_r = len(list_r)
+
+        list_p = list(df_new_i['Part']) + list(df_new_j['Part'])
+        u_p = len(remove_duplicates(list_p))
+        n_p = len(list_p)
+
+        list_i = list(df_new_i['Input']) + list(df_new_j['Input'])
+        u_i = len(remove_duplicates(list_i))
+        n_i = len(list_i)
+
+
+        print(u_a,n_a)
+        print(df_new_i)
+        print(df_new_j)
 
 window.close()
